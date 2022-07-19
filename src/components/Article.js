7d4488fdf02c0aec83cd58542425a66c034daf04
Wairimu2018 <wairimukuria.wk@gmail.com>
2022-07-19 16:19:13 +0300

    Initial Commit

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,8 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      {/* uncomment the line below to pass the test */}
+      <p>please pass this test</p>
     </div>
   );
 }
